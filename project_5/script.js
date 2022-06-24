0e6028ec795a8f8609cabe717780b410d0f27396
seanjaculina <seanjaculina1@gmail.com>
2022-06-23 17:11:44 -0700

    project 5 completion

---
+++ project_5/script.js
@@ -0,0 +1,29 @@
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+
+let load = 0;
+
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+  }
+
+blurring = () => {
+    load++;
+
+    if(load > 99){
+        clearInterval(interval); // stop the incrementation of the load variable once it gets greater than 99
+    }
+
+    loadText.innerText = `${load}%`;
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+
+
+}
+
+let interval = setInterval(blurring, 30); /* 30 ms*/
+
+
+
