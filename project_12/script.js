417e5a6e9765055515e95e1fef00cd048943ec35
seanjaculina <seanjaculina1@gmail.com>
2022-07-20 12:19:19 -0700

    project 11 / 12 finished

---
+++ project_12/script.js
@@ -0,0 +1,9 @@
+// select all the toggle buttons
+let toggleBtn =  document.querySelectorAll('.faq-toggle');
+
+// loop through each toggle button
+toggleBtn.forEach((btn)=>{
+    btn.addEventListener('click', ()=>{
+        btn.parentNode.classList.toggle('active');
+    })
+})~
